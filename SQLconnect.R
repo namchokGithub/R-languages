e2e37f7f0d0e3bd8183969eb1239cd3d5438efab
namchokGithub <pech4751@gmail.com>
2019-12-27 11:22:09 +0700

    quiz

--- SQLconnect.R
+++ SQLconnect.R
@@ -1,15 +1,38 @@
 library(RMySQL)
 
+# utf8
+Sys.setlocale("LC_CTYPE", "UTF-8")
+# options(encoding="UTF-8")
+
+
 #------------ Connect database -------------------#
 
   con = dbConnect(MySQL(),
-                  user="team2",
-                  password="team2@tsp59",
-                  dbname="r_database",
-                  host="10.80.39.191")
+                  user="oesuser",
+                  password="tsp60@oesuser",
+                  dbname="tsp60_nu_oesdb",
+                  host="10.80.39.17")
   
-  dataFormDB <- dbGetQuery(con, "SELECT * FROM music_record")
+  dataFormDB <- dbGetQuery(con, "SELECT * FROM oes_result_exam")
+  
   dbDisconnect(con)
   
 #------------ Connect database -------------------#
+  
+  
+# user: oesuser  
+# pass: tsp60@oesuser
+  
+  con = dbConnect(MySQL(),
+                  user="oesuser",
+                  password="tsp60@oesuser",
+                  dbname="tsp60_nu_srpdb",
+                  host="10.80.39.17")
+  
+  dataFormDB <- dbGetQuery(con, "SELECT * FROM Amphur")
+  
+  dbDisconnect(con)
+  
+  
+  
   
---
+++ quiz.R
@@ -0,0 +1,15 @@
+library(readr)
+
+# Exel ,spreadsheets
+library(readxl)
+
+# Statustucak packeages
+library(haven)
+
+# Selecting variable
+library(dplyr)
+
+# reshape data
+library(tidyr)
+
+#------------------ End library -------------------- #
