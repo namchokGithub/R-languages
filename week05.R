46f59685730762c383dc58f1d64fc421ecbf4fab
namchokGithub <pech4751@gmail.com>
2020-01-10 09:37:37 +0700

    week 05

---
+++ week05.R
@@ -0,0 +1,8 @@
+library(readr)
+library(readxl) # Exel ,spreadsheets
+library(haven) # Statustucak packeages
+library(dplyr) # Selecting variable
+library(tidyr) # reshape data
+library(ggplot2) # Graph
+
+# ------------------------------------------------------- #
