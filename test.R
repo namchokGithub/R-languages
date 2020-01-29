786f53e150b9da25cda85d946756ada480b7b23f
Namchok <pech4751@gmail.com>
2020-01-29 15:08:36 +0700

    add library

--- test.R
+++ test.R
@@ -1,9 +1,31 @@
 library(readr)
 # Exel ,spreadsheets
+
 library(readxl)
 # Statustucak packeages
+
 library(haven)
 # Selecting variable
-library(dplyr)
+
+library(readr)
+# Read file etc. csv
+
+library(haven) 
+# Statustucak packeages
+
+library(dplyr) 
+# Selecting variable
+
+library(tidyr) 
+# reshape data
+
+library(ggplot2) 
+# Graph
+
+library(treemapify) 
+# Tree map
+
+library(scales) 
+# Percent.
 
 #------------------ End library -------------------- #
